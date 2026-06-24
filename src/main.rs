9ef4a1fe1233f60269bf941e6e2f07189186b560
Syn3x <1972567225@qq.com>
2026-06-24 15:11:44 +0800

    feat: init basic components

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, Bypass!");
+use axum::{Router, routing::get};
+use std::env;
+use tokio::net::TcpListener;
+use tower_http::trace::TraceLayer;
+use tracing::Level;
+use tracing_subscriber::FmtSubscriber;
+
+fn parse_log_level() -> Level {
+    let level = env::var("log_level")
+        .ok()
+        .and_then(|v| v.parse::<u8>().ok())
+        .unwrap_or(2); // default INFO
+
+    match level {
+        0 => Level::TRACE,
+        1 => Level::DEBUG,
+        2 => Level::INFO,
+        3 => Level::WARN,
+        4 => Level::ERROR,
+        _ => Level::INFO,
+    }
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // load env
+    dotenvy::dotenv().ok();
+
+    // set tracing
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(parse_log_level())
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber)?;
+
+    // auxm init
+    let app = Router::new()
+        .route("/", get(|| async { "Hello,Bypass" }))
+        // 日志中间件
+        .layer(TraceLayer::new_for_http());
+
+    let listener = TcpListener::bind(env::var("listener")?).await.unwrap();
+
+    tracing::info!("server started at {}", env::var("listener").unwrap());
+
+    axum::serve(listener, app).await.unwrap();
+
+    Ok(())
 }
